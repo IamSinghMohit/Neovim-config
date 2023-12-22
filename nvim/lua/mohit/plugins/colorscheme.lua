325d4c778a783df812fc5887e587aa04cc905581
MohitNishad <nishadamar989@gmail.com>
2023-12-22 08:50:30 +0530

    initial commit

---
+++ nvim/lua/mohit/plugins/colorscheme.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"olimorris/onedarkpro.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("onedarkpro").setup({
+				styles = {
+					types = "NONE",
+					methods = "NONE",
+					numbers = "NONE",
+					strings = "NONE",
+					comments = "italic",
+					keywords = "NONE",
+					constants = "NONE",
+					functions = "NONE",
+					operators = "NONE",
+					variables = "NONE",
+					parameters = "italic",
+					conditionals = "italic",
+					virtual_text = "italic",
+				},
+			})
+			vim.cmd([[colorscheme onedark]])
+		end,
+	},
+}
